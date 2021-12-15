4f4f7eb4224ed5a2e3c1b2b446812d6d5064a8b4
Oliver Stark <os@fortrabbit.com>
2021-12-15 13:52:39 +0000

    configure.php tweaks

--- configure.php
+++ configure.php
@@ -81,11 +81,10 @@ $vendorNamespace = ask('Vendor namespace', $vendorNamespace);
 $currentDirectory = getcwd();
 $folderName = basename($currentDirectory);
 
-$packageName = ask('Plugin name', $folderName);
+$packageName = ask('Composer package name', $folderName);
 $packageSlug = slugify($packageName);
 $pluginHandle = remove_prefix('craft-', $packageSlug);
-$className = title_case($packageName);
-$className = ask('Class name', $className);
+$className = ask('Class name', 'Plugin');
 $description = ask('Plugin description', "This is my plugin {$packageSlug}");
 $pluginLicense = ask('Plugin license (MIT or proprietary)', 'MIT');
 
@@ -122,6 +121,7 @@ foreach ($files as $file) {
         'package_slug' => $packageSlug,
         'plugin_handle' => $pluginHandle,
         'plugin_license' => $pluginLicense,
+        'SkeletonPlugin' => $className,
         'Skeleton' => $className,
         'skeleton' => $packageSlug,
         'package_description' => $description,
